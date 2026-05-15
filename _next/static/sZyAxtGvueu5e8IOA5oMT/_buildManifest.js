f360d90d67454f07a8d6afc2dde33c01f8aa534d
garrettgizen <garrettgizen@users.noreply.github.com>
2026-05-15 19:02:37 +0000

    deploy: 30a37dedb77d0acc9ae21837e982ef9d2dcaa96f

--- _next/static/sZyAxtGvueu5e8IOA5oMT/_buildManifest.js
+++ _next/static/sZyAxtGvueu5e8IOA5oMT/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/gizengraphics//_next/:path+",
+        "destination": "/gizengraphics/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
