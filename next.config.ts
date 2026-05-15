00d5856033b648a8ee4fd1dc2136b7fa4db76b6c
Garrett Gizen <garrettgiz0@gmail.com>
2026-05-15 12:54:58 -0600

    Remove base path, using root GitHub Pages domain

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  assetPrefix: "/Portfolio/",
-  basePath: "/Portfolio",
   trailingSlash: true,
 };
 
