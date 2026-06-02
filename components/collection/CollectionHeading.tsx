604de0fb62c250dd7799c076ae496762eaf91127
Garrett Gizen <garrettgizen@proton.me>
2026-06-02 14:38:14 -0600

    Update heading

--- components/collection/CollectionHeading.tsx
+++ components/collection/CollectionHeading.tsx
@@ -7,8 +7,10 @@ interface CollectionHeadingProps {
 
 export default function CollectionHeading({ data }: CollectionHeadingProps) {
   return (
-    <div className="flex flex-col flex-wrap gap-x-4 pt-32 px-4 tablet:px-8 pb-16">
-      <h2 className="font-heading text-5xl font-extrabold">{data.title}</h2>
+    <div className="flex flex-col flex-wrap gap-x-4 pt-24 tablet:pt-32 px-4 tablet:px-8 pb-16">
+      <h2 className="font-heading text-5xl font-extrabold mb-2">
+        {data.title}
+      </h2>
 
       <p className="opacity-70 text-lg font-normal">{data.description}</p>
     </div>
